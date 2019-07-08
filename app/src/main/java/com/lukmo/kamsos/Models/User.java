0730e7ab3202f718d51b8dfc9817fb718646c1e6
fatahrez <fertahrez@gmail.com>
2019-07-08 16:18:01 +0300

    add model for user

---
+++ app/src/main/java/com/lukmo/kamsos/Models/User.java
@@ -0,0 +1,25 @@
+package com.lukmo.kamsos.Models;
+
+public class User {
+
+    private String username;
+    private String password;
+    private String token;
+
+    public User (String username, String password){
+        this.username = username;
+        this.password = password;
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public String getToken() {
+        return token;
+    }
+}
