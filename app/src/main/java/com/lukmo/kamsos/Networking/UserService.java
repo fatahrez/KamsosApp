b56922ac416ec46a9735c87d1955267a0bbf4b05
fatahrez <fertahrez@gmail.com>
2019-07-17 13:34:04 +0300

    Edit Networking/UserService to add RxJava and add methods register() and login()

--- app/src/main/java/com/lukmo/kamsos/Networking/UserService.java
+++ app/src/main/java/com/lukmo/kamsos/Networking/UserService.java
@@ -1,11 +1,17 @@
 package com.lukmo.kamsos.Networking;
 
+import com.lukmo.kamsos.Models.Response;
 import com.lukmo.kamsos.Models.User;
 
 import retrofit2.Call;
+import retrofit2.http.Body;
 import retrofit2.http.POST;
+import rx.Observable;
 
 public interface UserService {
+    @POST("/auth/createpastoralist")
+    Observable<Response> register(@Body User user)
+
     @POST("/auth/login")
-    Call<User> me();
+    Observable<Response> login();
 }
