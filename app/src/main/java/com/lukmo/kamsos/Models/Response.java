144b65ebced47d4b14faf54e9f96a5bbccfba5b4
fatahrez <fertahrez@gmail.com>
2019-07-17 13:08:53 +0300

    finish writing model for Response

--- app/src/main/java/com/lukmo/kamsos/Models/Response.java
+++ app/src/main/java/com/lukmo/kamsos/Models/Response.java
@@ -1,4 +1,14 @@
 package com.lukmo.kamsos.Models;
 
 public class Response {
+    private String message;
+    private String token;
+
+    public String getMessage(){
+        return message;
+    }
+
+    public String getToken(){
+        return token;
+    }
 }
