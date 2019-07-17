cb60aa539fd9a47e16fb4c9131d5d0e78f538049
fatahrez <fertahrez@gmail.com>
2019-07-17 13:14:19 +0300

    in Utils/Constants add Constant Strings BASE_URL, TOKEN and EMAIL

--- app/src/main/java/com/lukmo/kamsos/Utils/Constants.java
+++ app/src/main/java/com/lukmo/kamsos/Utils/Constants.java
@@ -1,4 +1,7 @@
 package com.lukmo.kamsos.Utils;
 
 public class Constants {
+    public static final String BASE_URL = "https://kamsos.herokuapp.com/api/v1/";
+    public static final String TOKEN = "token";
+    public static final String EMAIL = "email";
 }
