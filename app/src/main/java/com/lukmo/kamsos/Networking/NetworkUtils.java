e792f2362a69db0369384940d6f78ba2cd7818af
fatahrez <fertahrez@gmail.com>
2019-07-24 19:20:19 +0300

    refactor NetworkUtils and write ApiInstance to initialiase UserService instance

--- app/src/main/java/com/lukmo/kamsos/Networking/NetworkUtils.java
+++ app/src/main/java/com/lukmo/kamsos/Networking/NetworkUtils.java
@@ -3,9 +3,9 @@ package com.lukmo.kamsos.Networking;
 public class NetworkUtils {
     private static UserService userService;
 
-    public static UserService ApiInstance(String email, String password){
+    public static UserService ApiInstance(){
         if (userService == null){
-            userService = ServiceGenerator.getUser(email, password).create(UserService.class);
+            userService = ServiceGenerator.getUser().create(UserService.class);
         }
         return userService;
     }
