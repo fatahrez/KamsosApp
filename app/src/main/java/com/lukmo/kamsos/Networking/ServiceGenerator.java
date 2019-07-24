01349aa909e9082d123f8d269cff33381e3ee8e8
fatahrez <fertahrez@gmail.com>
2019-07-24 19:18:33 +0300

    refactor Network/RetrofitAdapter.java to to wrote Retrofit instance for getUser()

--- app/src/main/java/com/lukmo/kamsos/Networking/ServiceGenerator.java
+++ app/src/main/java/com/lukmo/kamsos/Networking/ServiceGenerator.java
@@ -6,14 +6,9 @@ import com.google.gson.Gson;
 import com.google.gson.GsonBuilder;
 import com.lukmo.kamsos.Utils.Constants;
 
-import okhttp3.MediaType;
-import okhttp3.OkHttpClient;
-import okhttp3.Request;
-import okhttp3.RequestBody;
 import retrofit2.Retrofit;
 import retrofit2.adapter.rxjava.RxJavaCallAdapterFactory;
 import retrofit2.converter.gson.GsonConverterFactory;
-import rx.schedulers.Schedulers;
 
 public class ServiceGenerator {
     private static Retrofit retrofit;
@@ -25,11 +20,9 @@ public class ServiceGenerator {
             }
 
 
-            RxJavaCallAdapterFactory rxAdapter = RxJavaCallAdapterFactory.createWithScheduler(Schedulers.io());
-
             retrofit = new Retrofit.Builder()
                     .baseUrl(Constants.BASE_URL)
-                    .addCallAdapterFactory(rxAdapter)
+                    .addCallAdapterFactory(RxJavaCallAdapterFactory.create())
                     .addConverterFactory(GsonConverterFactory.create(gson))
                     .build();
         }
@@ -37,62 +30,4 @@ public class ServiceGenerator {
         return retrofit;
     }
 
-
-    public static synchronized Retrofit getUser(String email, String password){
-        String credentials = email + ":" + password;
-        String basic = "Bearer " + Base64.encodeToString(credentials.getBytes(),Base64.NO_WRAP);
-
-        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
-
-
-        httpClient.addInterceptor(chain -> {
-            Request original = chain.request();
-            Request.Builder builder = original.newBuilder()
-                    .addHeader("Authorization", basic)
-                    .method(original.method(), original.body());
-            return chain.proceed(builder.build());
-        });
-
-        RxJavaCallAdapterFactory rxAdapter = RxJavaCallAdapterFactory.createWithScheduler(Schedulers.io());
-
-        if (retrofit == null) {
-            if (gson == null) {
-                gson = new GsonBuilder().setLenient().create();
-            }
-
-            retrofit = new Retrofit.Builder()
-                    .baseUrl(Constants.BASE_URL)
-                    .client(httpClient.build())
-                    .addCallAdapterFactory(rxAdapter)
-                    .addConverterFactory(GsonConverterFactory.create(gson))
-                    .build();
-        }
-        return retrofit;
-    }
-
-    public static synchronized Retrofit getUser(String token){
-        OkHttpClient.Builder httpClient = new OkHttpClient.Builder();
-
-        httpClient.addInterceptor(chain -> {
-            Request original = chain.request();
-            Request.Builder builder = original.newBuilder()
-                    .addHeader("x-access-token", token)
-                    .method(original.method(), original.body());
-            return chain.proceed(builder.build());
-        });
-
-        RxJavaCallAdapterFactory rxAdapter = RxJavaCallAdapterFactory.createWithScheduler(Schedulers.io());
-        if (retrofit == null) {
-            if (gson == null) {
-                gson = new GsonBuilder().setLenient().create();
-            }
-            retrofit = new Retrofit.Builder()
-                    .baseUrl(Constants.BASE_URL)
-                    .client(httpClient.build())
-                    .addCallAdapterFactory(rxAdapter)
-                    .addConverterFactory(GsonConverterFactory.create(gson))
-                    .build();
-        }
-        return retrofit;
-    }
 }
