8c60a9504f9798ef76033c27c86af97bc159c566
fatahrez <fertahrez@gmail.com>
2019-07-24 18:39:56 +0300

    edit Networking/UserService to allow to post necessary data to API

--- app/src/main/java/com/lukmo/kamsos/Networking/UserService.java
+++ app/src/main/java/com/lukmo/kamsos/Networking/UserService.java
@@ -5,13 +5,21 @@ import com.lukmo.kamsos.Models.User;
 
 import io.reactivex.Observable;
 import retrofit2.http.Body;
+import retrofit2.http.Field;
 import retrofit2.http.Headers;
 import retrofit2.http.POST;
 
 public interface UserService {
     @POST("/auth/createpastoralist")
-    Observable<Response> register(@Body User user);
+    Observable<User> register(
+            @Field("username") String username,
+            @Field("email") String email,
+            @Field("password") String password
+    );
 
     @POST("/auth/login")
-    Observable<Response> login();
+    Observable<User> login(
+            @Field("email") String email,
+            @Field("password") String password
+    );
 }
