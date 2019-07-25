d605e4e5c5aba477a0c41f66bf75321553fcaeff
fatahrez <fertahrez@gmail.com>
2019-07-25 22:34:36 +0300

    Write Register Model in kamsos/Models

--- app/src/main/java/com/lukmo/kamsos/Models/Register/Register.java
+++ app/src/main/java/com/lukmo/kamsos/Models/Register/Register.java
@@ -1,4 +1,27 @@
 package com.lukmo.kamsos.Models.Register;
 
+import com.google.gson.annotations.Expose;
+import com.google.gson.annotations.SerializedName;
+import org.apache.commons.lang.builder.ToStringBuilder;
+
 public class Register {
+
+    @SerializedName("user")
+    @Expose
+    private Register_ user;
+
+    public Register_ getUser() {
+        return user;
+    }
+
+    public void setUser(Register_ user) {
+        this.user = user;
+    }
+
+    @Override
+    public String toString() {
+        return new ToStringBuilder(this).append("user", user).toString();
+    }
+
 }
+
