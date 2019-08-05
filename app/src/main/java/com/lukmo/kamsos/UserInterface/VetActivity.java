f5c42061faeb27136ad35faa9ea5b4614222f643
fatahrez <fertahrez@gmail.com>
2019-08-05 22:03:30 +0300

    add vet recyclerview to activity_vet.xml

--- app/src/main/java/com/lukmo/kamsos/UserInterface/VetActivity.java
+++ app/src/main/java/com/lukmo/kamsos/UserInterface/VetActivity.java
@@ -2,14 +2,39 @@ package com.lukmo.kamsos.UserInterface;
 
 import android.os.Bundle;
 
+import com.lukmo.kamsos.Models.Vet.Vet;
+import com.lukmo.kamsos.Presenters.UserPresenter;
 import com.lukmo.kamsos.R;
+import com.lukmo.kamsos.UserInfrastructure.UserInfrastructure;
 
-public class VetActivity extends Main2Activity {
+import java.util.List;
+
+public class VetActivity extends Main2Activity implements UserInfrastructure.View{
+    private UserInfrastructure.Presenter mPresenter;
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_vet);
+
+        mPresenter = new UserPresenter(this);
+        mPresenter.start();
+    }
+
+
+
+    @Override
+    public void init() {
+        mPresenter.loadVets();
+    }
+
+    @Override
+    public void showError(String message) {
+
+    }
+
+    @Override
+    public void loadDataInList(List<Vet> vets) {
 
     }
 
