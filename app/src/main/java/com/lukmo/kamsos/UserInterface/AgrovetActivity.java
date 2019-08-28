fed74b03a5a7413f3259b31e58f253515c25ccb7
Wess58 <wesleymbate@gmail.com>
2019-08-28 12:05:20 +0300

    removed bottom Nav

--- app/src/main/java/com/lukmo/kamsos/UserInterface/AgrovetActivity.java
+++ app/src/main/java/com/lukmo/kamsos/UserInterface/AgrovetActivity.java
@@ -3,17 +3,21 @@ package com.lukmo.kamsos.UserInterface;
 
 import android.os.Bundle;
 
+import androidx.appcompat.app.AppCompatActivity;
+
 import com.lukmo.kamsos.R;
 
-public class AgrovetActivity extends Main2Activity {
+public class AgrovetActivity extends AppCompatActivity {
 
     @Override
     public void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_agrovet);
+
 
     }
 
-    @Override
+    /*@Override
     int getContentViewId() {
         return R.layout.activity_agrovet;
     }
@@ -21,5 +25,5 @@ public class AgrovetActivity extends Main2Activity {
     @Override
     int getNavigationMenuItemId() {
         return R.id.navigation_agrovets;
-    }
+    }*/
 }
