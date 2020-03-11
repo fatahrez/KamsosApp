10ccb2c470e3ba5935f9e838d4ca2a0dfaf827f3
fatahrez <fertahrez@gmail.com>
2020-03-11 14:16:17 +0300

    in Presenters/VetDetailPresenter add Constructor VetDetailPresenter() and instance of VetDetailsInfrastructure.View

--- app/src/main/java/com/lukmo/kamsos/Presenters/VetDetailPresenter.java
+++ app/src/main/java/com/lukmo/kamsos/Presenters/VetDetailPresenter.java
@@ -3,10 +3,15 @@ package com.lukmo.kamsos.Presenters;
 import com.lukmo.kamsos.UserInfrastructure.VetDetailsInfrastructure;
 
 public class VetDetailPresenter implements VetDetailsInfrastructure.Presenter {
+    VetDetailsInfrastructure.View mView;
+
+    public VetDetailPresenter(VetDetailsInfrastructure.View mView){
+        this.mView = mView;
+    }
 
     @Override
     public void start() {
-
+        mView.init();
     }
 
     @Override
