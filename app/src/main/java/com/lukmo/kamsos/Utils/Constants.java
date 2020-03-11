e3defc97401d35fb8e4479f197cb55c6dfb3b487
fatahrez <fertahrez@gmail.com>
2020-03-11 12:44:46 +0300

    fix API issue for api.kamsos.com. It works good now with api.kamsos.com:8080

--- app/src/main/java/com/lukmo/kamsos/Utils/Constants.java
+++ app/src/main/java/com/lukmo/kamsos/Utils/Constants.java
@@ -1,7 +1,7 @@
 package com.lukmo.kamsos.Utils;
 
 public class Constants {
-    public static final String BASE_URL = "https://kamsos.herokuapp.com/api/v1/";
+    public static final String BASE_URL = "http://api.kamsos.com:8080/api/v1/";
     public static final String TOKEN = "token";
     public static final String EMAIL = "email";
 }
