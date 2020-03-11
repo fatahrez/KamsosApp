8393a12946373bdb1c4c9aeafb6742816cb15642
fatahrez <fertahrez@gmail.com>
2020-03-11 14:10:06 +0300

    created VetDetailsPresenter in presenters and implement VetDetailsInfrastructure.Presenter

---
+++ app/src/main/java/com/lukmo/kamsos/Presenters/VetDetailPresenter.java
@@ -0,0 +1,16 @@
+package com.lukmo.kamsos.Presenters;
+
+import com.lukmo.kamsos.UserInfrastructure.VetDetailsInfrastructure;
+
+public class VetDetailPresenter implements VetDetailsInfrastructure.Presenter {
+
+    @Override
+    public void start() {
+
+    }
+
+    @Override
+    public void loadVetDetails() {
+
+    }
+}
